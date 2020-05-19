6effd432373081d6b5bf866fab35e84843ab965b
Robert Devereux <robert.devereux3@mail.dcu.ie>
2020-05-18 21:21:42 -0700

    maven boilerplate

---
+++ src/main/java/com/robdev/beniceornotbenice/BeNiceOrNotBeNiceService/BeNiceOrNotBeNiceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.robdev.beniceornotbenice.BeNiceOrNotBeNiceService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeNiceOrNotBeNiceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeNiceOrNotBeNiceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robdev/beniceornotbenice/BeNiceOrNotBeNiceService/BeNiceOrNotBeNiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robdev.beniceornotbenice.BeNiceOrNotBeNiceService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeNiceOrNotBeNiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
